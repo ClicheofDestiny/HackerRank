a46ebe575656f4d64cc74d9b8f776c0e644ccff4
ClicheofDestiny <clicheofdestiny@gmail.com>
2016-07-14 19:21:20 +1000

    Solved in linear time

--- DivisibleSumPairs.cpp
+++ DivisibleSumPairs.cpp
@@ -9,19 +9,17 @@ int main(){
   int N, K;
   std::cin >> N >> K;
   int count = 0;
-  std::vector<int> nums (N);
+  int num;
+  int res = (-5)%8;
   for(int i = 0; i < N; i++){
-    std::cin >> nums[i];
-    all[nums[i]/K]++;
-  }
-  for(int i = 0; i < N-1; i++){
-    for(int j = i+1; j < N; j++){
-      if((nums[i]+nums[j]) % K == 0){
-        count++;
-      } 
+    std::cin >> num;
+    if(num%K == 0){
+      count += all[0];
+    } else {
+      count += all[K - (num % K)];
     }
+    all[num % K]++;
   }
-    
   std::cout << count << std::endl;
   return 0;
 }
